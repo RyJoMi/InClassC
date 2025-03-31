1906166a8922df5851090a37bbf1c2bcd5bfc55b
RyJoMi <tup88794@temple.edu>
2025-03-31 15:45:20 -0400

    s

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //Let's make some objects!
+
+//Let's make a water bottle object
+const waterBottle = {
+  color: "green",
+  volume: 14,
+  isOpen: false,
+  lidType: "screw",
+};
+
+console.log(waterBottle);
